502852144fd657625a47cdd5c5b93d06e284a18a
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-11-07 16:57:48 +0500

    Initial commit

---
+++ src/main/java/com/mark/amoCrmTask/AmoCrmTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mark.amoCrmTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoCrmTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmoCrmTaskApplication.class, args);
+	}
+
+}
